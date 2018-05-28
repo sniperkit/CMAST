64b2d90dfcc91e434ec69a61d16dd734b171a8e0
Justin Bassett <jbassett271@gmail.com>
2018-05-28 16:43:36 -0700

    Try ${TOOLCHAIN} instead of $TOOLCHAIN

--- .ci/install-cmake.sh
+++ .ci/install-cmake.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CMAKE_URL="https://cmake.org/files/v3.11/cmake-3.11.2.tar.gz"
+CMAKE_URL="https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.tar.gz"
 mkdir cmake
 travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
 export PATH=${CMAKE_DIR}/cmake/bin:${PATH}
