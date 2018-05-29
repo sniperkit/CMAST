40fd31edd142a8d08db23e467bb1caeb333fc21a
Justin Bassett <jbassett271@gmail.com>
2018-05-28 19:15:49 -0700

    remove non-toolchain things

---
+++ .ci/conan.toolchain.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_BUILD_TYPE Debug)
+
+macro(find_package NAME)
+  if(NOT _TOOLCHAIN_CONAN_SETUP)
+    set(_TOOLCHAIN_CONAN_SETUP ON)
+    include("${CMAKE_BINARY_DIR}/conanbuildinfo.cmake")
+    conan_define_targets()
+    conan_set_find_paths()
+  endif()
+
+  _find_package(${ARGV})
+endmacro()
--- .ci/gcc.toolchain.cmake
+++ .ci/gcc.toolchain.cmake
@@ -2,14 +2,14 @@ set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_EXTENSIONS OFF)
 set(CMAKE_BUILD_TYPE Debug)
 
-add_compile_options(
-  -Wall
-  -Wextra
-  -Wpedantic
-  # Warnings as error for CI builds
-  -Werror
-)
-link_libraries(stdc++fs)
+# add_compile_options(
+#   -Wall
+#   -Wextra
+#   -Wpedantic
+#   # Warnings as error for CI builds
+#   -Werror
+# )
+# link_libraries(stdc++fs)
 
 macro(find_package NAME)
   if(NOT _TOOLCHAIN_CONAN_SETUP)
